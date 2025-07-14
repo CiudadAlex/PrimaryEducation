9a00b5a56fcb9afffca771a524fb9f6605b577dd
CiudadAlex <ciudad.alex@gmail.com>
2025-07-15 01:13:28 +0200

    PrimaryEducation

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ from util.HtmlCrawler import HtmlCrawler
 url = 'https://es.wiktionary.org/wiki/Ap%C3%A9ndice:1000_palabras_b%C3%A1sicas_en_espa%C3%B1ol'
 
 soup = HtmlCrawler.get_soup_handle(url)
-tags_con_propiedad = soup.find(id="bodyContent")
+html_tag = soup.find(id="bodyContent")
+html_tag = html_tag.find(id="mw-content-text")
 
-# Mostrar los resultados
-for tag in tags_con_propiedad:
-    print(tag)
+sections = html_tag.find_all('section')
+
+for s in sections:
+    print(s.text)
+
