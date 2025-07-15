eefda9cb2499c75ff5b2cfbee34f1e38865f7574
CiudadAlex <ciudad.alex@gmail.com>
2025-07-15 02:12:36 +0200

    PrimaryEducation

---
+++ crawlers/SpanishBasicWordsCrawler.py
@@ -0,0 +1,50 @@
+from util.HtmlCrawler import HtmlCrawler
+from util.TextFilter import TextFilter
+
+
+class SpanishBasicWordsCrawler:
+
+    @staticmethod
+    def execute():
+
+        url = 'https://es.wiktionary.org/wiki/Ap%C3%A9ndice:1000_palabras_b%C3%A1sicas_en_espa%C3%B1ol'
+
+        soup = HtmlCrawler.get_soup_handle(url)
+        html_tag = soup.find(id="bodyContent")
+        html_tag = html_tag.find(id="mw-content-text")
+
+        sections = html_tag.find_all('section')
+        sections = sections[1:]
+
+        list_lines = []
+
+        for s in sections:
+            lines_section = s.text.splitlines()
+            for line in lines_section:
+                list_lines.append(line)
+
+        print(f"list_lines: {len(list_lines)}")
+
+        list_lines = TextFilter.remove_of_list_if_item_contains(list_lines, "[editar]")
+
+        print(f"list_lines: {len(list_lines)}")
+
+        list_lines = TextFilter.truncate_text_of_list(list_lines, ",")
+
+        print(f"list_lines: {len(list_lines)}")
+
+        list_lines = TextFilter.remove_of_list_if_text_is_short(list_lines, 5)
+
+        print(f"list_lines: {len(list_lines)}")
+
+        list_lines = TextFilter.remove_of_list_if_item_contains(list_lines, " ")
+
+        print(f"list_lines: {len(list_lines)}")
+
+        content = ""
+        for line in list_lines:
+            content = content + line + "\n"
+
+        with open('./corpus/SpanishBasicWords.txt', 'w', encoding='utf-8') as file:
+            file.write(content)
+
--- main.py
+++ main.py
@@ -1,40 +1,4 @@
-from util.HtmlCrawler import HtmlCrawler
-from util.TextFilter import TextFilter
+from crawlers.SpanishBasicWordsCrawler import SpanishBasicWordsCrawler
 
-url = 'https://es.wiktionary.org/wiki/Ap%C3%A9ndice:1000_palabras_b%C3%A1sicas_en_espa%C3%B1ol'
-
-soup = HtmlCrawler.get_soup_handle(url)
-html_tag = soup.find(id="bodyContent")
-html_tag = html_tag.find(id="mw-content-text")
-
-sections = html_tag.find_all('section')
-sections = sections[1:]
-
-list_lines = []
-
-for s in sections:
-    lines_section = s.text.splitlines()
-    for line in lines_section:
-        list_lines.append(line)
-
-print(f"list_lines: {len(list_lines)}")
-
-list_lines = TextFilter.remove_of_list_if_item_contains(list_lines, "[editar]")
-
-print(f"list_lines: {len(list_lines)}")
-
-list_lines = TextFilter.truncate_text_of_list(list_lines, ",")
-
-print(f"list_lines: {len(list_lines)}")
-
-list_lines = TextFilter.remove_of_list_if_text_is_short(list_lines, 5)
-
-print(f"list_lines: {len(list_lines)}")
-
-list_lines = TextFilter.remove_of_list_if_item_contains(list_lines, " ")
-
-print(f"list_lines: {len(list_lines)}")
-
-for line in list_lines:
-    print(line)
+SpanishBasicWordsCrawler.execute()
 
