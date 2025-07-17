9eb4ba910b768a204c7e26c21c7fa2de9a62f176
CiudadAlex <ciudad.alex@gmail.com>
2025-07-17 14:41:36 +0200

    PrimaryEducation

--- tasks/SpanishStartReadingTask.py
+++ tasks/SpanishStartReadingTask.py
@@ -11,8 +11,34 @@ class SpanishStartReadingTask:
     def execute(self, number_of_words=10):
 
         list_random_words = RandomLineLoader.get_random_words(Constants.CORPUS_SPANISH_BASIC_WORDS, number_of_lines=number_of_words)
-        print(list_random_words)
+        self.print_in_log("WORDS", list_random_words)
 
-        response = self.llm.execute("hola como estas?")
-        print(response)
+        partition_words = SpanishStartReadingTask.partition_list(list_random_words, 3)
+
+        for list_words in partition_words:
+            response = self.call_llm(list_words)
+            self.print_in_log("RESPONSE", response)
+
+    def call_llm(self, list_words):
+
+        prompt = "Crea una frase no muy larga usando lenguaje sencillo que puedan entender un niño de primaria y en la que uses las palabras:"
+
+        for word in list_words:
+            prompt = prompt + " '" + word + "',"
+
+        self.print_in_log("PROMPT", prompt)
+
+        response = self.llm.execute(prompt)
+
+        return response
+
+    def print_in_log(self, title, obj):
+        print("#####################")
+        print(title)
+        print(obj)
+        print("#####################")
+
+    @staticmethod
+    def partition_list(list_obj, size):
+        return [list_obj[i:i + size] for i in range(0, len(list_obj), size)]
 
